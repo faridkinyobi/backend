493091656accb00c5454158f636fbe8fb0ed7590
rizkyizh <rizkiizzulhaq14@gmail.com>
2023-06-08 15:56:13 +0700

    add helper res body template & change response register and login using res body template

--- app/api/users/controler.js
+++ app/api/users/controler.js
@@ -1,97 +1,85 @@
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-import UserModal from "./modal.js"
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import UserModal from "./modal.js";
+import responseBody from "../../helpers/responseBody.js";
 
-export const getUser = async (req , res) =>{
-    try {
-        const users = await UserModal.findAll()
-        res.status(200).json({
-        data: users,
-        metadata: "test user endpoint"
-    })
-    } catch (e) {
-        res.status(400).json({
-        error: "invalid"
-        })
-    }
-}
-export const getUserById = (req , res) =>{
-    
-}
-export const createUser  = async(req , res) =>{
-}
-export const updateUser  = (req , res) =>{
-    
-}
-export const deleteUser  = (req , res) =>{
-    
-}
+export const getUser = async (req, res) => {
+  try {
+    const users = await UserModal.findAll();
+    res.status(200).json({
+      data: users,
+      metadata: "test user endpoint",
+    });
+  } catch (e) {
+    res.status(400).json({
+      error: "invalid",
+    });
+  }
+};
+export const getUserById = (req, res) => {};
+export const createUser = async (req, res) => {};
+export const updateUser = (req, res) => {};
+export const deleteUser = (req, res) => {};
 
 export const register = async (req, res) => {
-    const { name, email, password } = req.body
-    
-    try {
-        const user = await UserModal.findAll()
-        const duplicateName = user.find((user) => user.name === name)
+  const { name, email, password } = req.body;
 
-        if (duplicateName) {
-            res.status(409).json({
-                status: 'fail', message: 'name duplicate'
-            })
-        } else {
-            const hashPaswword = await bcrypt.hash(password, 10);
+  try {
+    const user = await UserModal.findAll();
+    const duplicateName = user.find((user) => user.name === name);
 
-            const result = await UserModal.create({
-                name,
-                email,
-                password: hashPaswword
-            })
+    if (duplicateName) {
+      responseBody(409, "Conflict", { message: "nama sudah ada!" }, res);
+    } else {
+      const hashPaswword = await bcrypt.hash(password, 10);
+      const result = await UserModal.create({
+        name,
+        email,
+        password: hashPaswword,
+      });
 
-            res.status(201).json({
-                status: 'success', message: 'register successfully', data: { id: result.dataValues.id }
-            })
-        }
-
-    } catch (e) {
-        res.status(400).json({
-            status: 'fail', message: `${e.name}: ${e.message}`
-        })
+      responseBody(
+        201,
+        "Created",
+        { message: "akun berhasil dibuat", data: { id: result.dataValues.id } },
+        res
+      );
     }
-}
+  } catch (e) {
+    responseBody(400, "fail", { message: `${e.name} : ${e.message}` }, res);
+  }
+};
 
 export const loginAuth = async (req, res) => {
-    const { email, password } = req.body
-    
-    try {
-        const foundUser = await UserModal.findOne({
-            where: { email }
-        })
-        
-        const user = foundUser.dataValues;
-        
-        if (!user) {
-            res.status(401).json({
-                status: 'fail', message: 'user not found'
-            })
-        }
+  const { email, password } = req.body;
 
-        const match = await bcrypt.compare(password, user.password);
+  try {
+    const foundUser = await UserModal.findOne({
+      where: { email },
+    });
 
-        if (match) {
-            const username = { name: user.name };
-            const token = jwt.sign(username, process.env.TOKEN, { expiresIn: '24h' });
+    const user = foundUser.dataValues;
 
-            res.status(200).json({
-                status: 'success', token
-            })
-        } else {
-            res.status(401).json({
-                status: 'fail', message: 'user not found'
-            })
-        }
-    } catch(e) {
-        res.status(404).json({
-            status: 'fail', message: 'user not found!'
-        })
+    if (!user) {
+      responseBody(404, "fail", { message: "user tidak ditemukan" }, res);
     }
-}
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (match) {
+      const username = { name: user.name };
+      const token = jwt.sign(username, process.env.TOKEN, { expiresIn: "24h" });
+
+      responseBody(
+        200,
+        "success",
+        { message: "berhasil login!", data: { token: token } },
+        res
+      );
+    } else {
+      responseBody(404, "fail", { message: "user tidak ditemukan" }, res);
+    }
+  } catch (e) {
+    responseBody(404, "fail", { message: "user tidak ditemukan" }, res);
+  }
+};
